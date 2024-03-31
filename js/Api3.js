6d0645e60aef49c660f2aeabe4b2c606454342ae
Fidel Ernesto Perez Bedoya <92316606+fidelpperez2021@users.noreply.github.com>
2024-03-30 19:59:53 -0500

    Add files via upload

---
+++ js/Api3.js
@@ -0,0 +1,33 @@
+const getPokemon = async () => {
+    let characters = await fetch(`https://rickandmortyapi.com/api/character?page=4`);
+    characters = await characters.json();
+    console.log(characters);
+    
+    if (characters.results){
+        for (let index = 0; index < characters.results.length; index++) {
+            const characterData = await fetch(characters.results[index].url);
+            characters.results[index].data = await characterData.json();
+        }
+    }
+    console.log(characters);
+    displayCharacters(characters.results);
+}
+
+function displayCharacters(characterList) {
+    const serviceItems = document.querySelectorAll('.service-item');
+    characterList.forEach((character, index) => {
+        if (index < serviceItems.length) {
+            const img = serviceItems[index].querySelector('.service-img img');
+            img.src = character.data.image;
+            img.alt = character.data.name;
+
+            const serviceName = serviceItems[index].querySelector('.service-content-inner h5');
+            serviceName.textContent = character.data.name;
+
+            const serviceDescription = serviceItems[index].querySelector('.service-content-inner p');
+            serviceDescription.textContent = `Status: ${character.data.status}, Species: ${character.data.species}, Type: ${character.data.type}`;
+        }
+    });
+}
+
+window.addEventListener('load', getPokemon);
